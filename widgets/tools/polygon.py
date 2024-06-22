d0ad25b0af3acad34f032340c46c637a21b1fe6b
Antraxmin <antraxmin@naver.com>
2024-06-22 17:00:09 +0900

    add mouse control

--- widgets/tools/polygon.py
+++ widgets/tools/polygon.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
 from PyQt5.QtCore import Qt, QPointF
-from PyQt5.QtGui import QPainter, QPen, QColor
+from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtWidgets import QGraphicsPolygonItem
 
 class PolygonTool(QGraphicsView):
@@ -14,3 +14,20 @@ class PolygonTool(QGraphicsView):
         self.pen.setWidth(2)
         self.polygon_item = None
         self.polygon_points = []
+
+    def mousePressEvent(self, event):
+        if not self.polygon_item:
+            self.polygon_item = QGraphicsPolygonItem()
+            self.polygon_item.setPen(self.pen)
+            self.scene.addItem(self.polygon_item)
+
+        point = self.mapToScene(event.pos())
+        self.polygon_points.append(point)
+        polygon = self.polygon_item.polygon()
+        polygon.append(QPointF(point.x(), point.y()))
+        self.polygon_item.setPolygon(polygon)
+
+    def mouseDoubleClickEvent(self, event):
+        if self.polygon_item:
+            self.polygon_item = None
+            self.polygon_points = []
