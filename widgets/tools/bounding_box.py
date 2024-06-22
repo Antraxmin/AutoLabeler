ee1ad12d782c9d2882b2c7b085079785f0761980
Antraxmin <antraxmin@naver.com>
2024-06-22 16:52:13 +0900

    add mouse click event

--- widgets/tools/bounding_box.py
+++ widgets/tools/bounding_box.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRectF
 from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtWidgets import QGraphicsRectItem
 
 class BoundingBoxTool(QGraphicsView):
     def __init__(self, parent=None):
@@ -13,3 +14,19 @@ class BoundingBoxTool(QGraphicsView):
         self.pen.setWidth(2)
         self.current_rect = None
         self.start_point = None
+
+    def mousePressEvent(self, event):
+        self.start_point = event.pos()
+        self.current_rect = QGraphicsRectItem()
+        self.current_rect.setPen(self.pen)
+        self.scene.addItem(self.current_rect)
+
+    def mouseMoveEvent(self, event):
+        if self.start_point:
+            end_point = event.pos()
+            rect = QRectF(self.start_point, end_point).normalized()
+            self.current_rect.setRect(rect)
+
+    def mouseReleaseEvent(self, event):
+        self.start_point = None
+        self.current_rect = None
